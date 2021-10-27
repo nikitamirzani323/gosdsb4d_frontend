b8a7e8a691e01a218ee4ecb0be3dc0d20fb8fb90
nikitamirzani323 <nikitamirzani323@gmail.com>
2021-10-28 06:12:41 +0700

    Logo

--- frontend/src/components/Navbar.svelte
+++ frontend/src/components/Navbar.svelte
@@ -9,6 +9,7 @@
 </script>
 <div class="container">
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <img style="margin-top:10px;" src="logo.png" alt="" />
         <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
             <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
         </a>
